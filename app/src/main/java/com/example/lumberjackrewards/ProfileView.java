449ed104fbf183c8d02650e787eeac9e4bf37e7c
Toluwaez <91505730+Toluwaez@users.noreply.github.com>
2024-02-12 14:56:18 -0600

    back button from the profile page
    I added the back button from the profile page to the home page.

--- app/src/main/java/com/example/lumberjackrewards/ProfileView.java
+++ app/src/main/java/com/example/lumberjackrewards/ProfileView.java
@@ -2,13 +2,28 @@ package com.example.lumberjackrewards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileView extends AppCompatActivity {
+
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        back = findViewById(R.id.backBtn);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
