dc1fe89e3f5f2bd25bea26b5147e89bf5d094552
Zxyphor <99300585+Zxyphor@users.noreply.github.com>
2024-04-12 17:23:14 -0500

    testing/editing for API integration

--- app/src/main/java/com/example/lumberjackrewards/Framework.java
+++ app/src/main/java/com/example/lumberjackrewards/Framework.java
@@ -7,8 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.sql.Blob;
-import java.net.*;
+import java.lang.Object;
+import java.net.http.*;
 import java.io.*;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -65,33 +70,28 @@ class ProfileInfo{
 
 class BadgeInfo{
     static String stream(int id, String arg) {
-        try {
-            String urlString = "cs.sfasu.edu/csci4267-00101/BackFrontEndStrikesBack/access/api/"+arg+id;
-            URL url = new URL(urlString);
-            InputStream input = url.openStream();
-            InputStreamReader isr = new InputStreamReader(input);
-            BufferedReader reader = new BufferedReader(isr);
-            StringBuilder json = new StringBuilder();
-            int c;
-            while ((c = reader.read()) != -1) {
-                json.append((char) c);
-            }
-            return json.toString();
-        } catch (Exception e) {
-            System.out.println("Failed try/catch stream fn in profileInfo.");
-        }
+         try {
+                URL url = new URL("https://cs.sfasu.edu/csci4267-00104/BackFrontEndStrikesBack/access/api/getUser.php?userID=1");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                while (in.available() > 0){
+                    System.out.print((char)in.read());
+                }
+         } catch (Exception e) {
+                System.out.println("stupid crap failed" + e.toString());
+         }
         return "Failed after try/catch stream fn in profileInfo.";
 
     }
 
-    public static int getID(){
-        return 0;
+    public static int getID(int id){
+        return id;
     }
     public static String getName(int id){
-        String output = stream(id, "getUser.php?badgeID=");
-        return output;
+
+        return stream(id, "getUser.php?badgeID=");
     }
-    public static int getProfilepic(){
+    public static int getProfilepic(int id){
         return 0;
     }
 
