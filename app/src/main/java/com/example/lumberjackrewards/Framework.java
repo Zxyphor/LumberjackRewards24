625b6b24e57baa97b0757fada8cce912629e62f9
Zxyphor <99300585+Zxyphor@users.noreply.github.com>
2024-04-12 18:20:09 -0500

    Framework first implementation
    Added class structure for all known API calls. BadgeInfo and ProfileInfo should be fully implemented. All other calls are empty until verified working again

--- app/src/main/java/com/example/lumberjackrewards/Framework.java
+++ app/src/main/java/com/example/lumberjackrewards/Framework.java
@@ -13,17 +13,13 @@ import java.net.URI;
 import java.net.URL;
 import java.sql.Blob;
 import java.lang.Object;
-import java.net.http.*;
 import java.io.*;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-//pseudo-DB
-//structure for storing profile information from DB
-class ProfileInfo{
-
+class APICaller{
+    //Function to use API calls
+    //single arg
     static String stream(int id, String arg) {
         try {
             String urlString = "cs.sfasu.edu/csci4267-00101/BackFrontEndStrikesBack/access/api/"+arg+id;
@@ -41,87 +37,100 @@ class ProfileInfo{
             System.out.println("Failed try/catch stream fn in profileInfo.");
         }
         return "Failed after try/catch stream fn in profileInfo.";
-
     }
 
-    public static int getID(){
-        return 0;
-    }
-    public static String getName(int id){
-        String output = stream(id, "getUser.php?userID=");
-        return "";
-    }
-    public static int getProfilepic(){
-        return 0;
+    //double arg
+    static String stream(int id, String arg, int id2, String arg2) {
+        try {
+            String urlString = "cs.sfasu.edu/csci4267-00101/BackFrontEndStrikesBack/access/api/"+arg+id+arg2+id2;
+            URL url = new URL(urlString);
+            InputStream input = url.openStream();
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            return json.toString();
+        } catch (Exception e) {
+            System.out.println("Failed try/catch stream fn in profileInfo.");
+        }
+        return "Failed after try/catch stream fn in profileInfo.";
     }
 
-    /*
-    public int getID(int id){
-        return BadgeInfo(id).id;
-    }
-    public String getName(int id){
-        return BadgeInfo(id).name;
-    }
-    public int getProfilepic(int id){
-        return BadgeInfo(id).profilepic;
-    }
-    */
+
 }
 
-class BadgeInfo{
-    static String stream(int id, String arg) {
-         try {
-                URL url = new URL("https://cs.sfasu.edu/csci4267-00104/BackFrontEndStrikesBack/access/api/getUser.php?userID=1");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                while (in.available() > 0){
-                    System.out.print((char)in.read());
-                }
-         } catch (Exception e) {
-                System.out.println("stupid crap failed" + e.toString());
-         }
-        return "Failed after try/catch stream fn in profileInfo.";
-
-    }
-
+//Set of commands for pulling a single user's information
+class ProfileInfo extends APICaller{
     public static int getID(int id){
         return id;
     }
     public static String getName(int id){
-
-        return stream(id, "getUser.php?badgeID=");
+        String in =  stream(id, "getUser.php?userID=");
+        String[] outAry = in.split("\"");
+        return outAry[5];
+    }
+    public static String getLastLogin(int id){
+        String in =  stream(id, "getUser.php?userID=");
+        String[] outAry = in.split("\"");
+        return outAry[9];
     }
     public static int getProfilepic(int id){
-        return 0;
+        String in =  stream(id, "getUser.php?userID=");
+        String[] outAry = in.split("\"");
+        return Integer.parseInt(outAry[13]);
     }
+}
 
-    /* When API is complete replace above with below
+//Set of commands for pulling a single badge's info
+class BadgeInfo extends APICaller{
+
     public int getID(int id){
-        return BadgeInfo(id).id
-    }
-    public String getDescription(int id){
-        return BadgeInfo(id).description;
+        return id;
     }
     public String getName(int id){
-        return BadgeInfo(id).name;
+        String in =  stream(id, "getBadge.php?badgeID=");
+        String[] outAry = in.split("\"");
+        return outAry[9];
     }
-    public String getIcon(int id){
-        return BadgeInfo(id).icon;
+    public String getDescription(int id){
+        String in =  stream(id, "getBadge.php?badgeID=");
+        String[] outAry = in.split("\"");
+        return outAry[13];
     }
-    public int getCompletionStatus(int id){
-        return BadgeInfo(id).completionStatus;
+    public String getCriteria(int id){
+        String in =  stream(id, "getBadge.php?badgeID=");
+        String[] outAry = in.split("\"");
+        return outAry[17];
     }
-    public int getSteps(int id){
-        return BadgeInfo(id).steps;
+    public int getIcon(int id){
+        String in =  stream(id, "getBadge.php?badgeID=");
+        String[] outAry = in.split("\"");
+        return Integer.parseInt(outAry[21]);
     }
-    public String getRequirements(int id){
-        return BadgeInfo(id).requirements;
+    public String getCreationDate(int id){
+        String in =  stream(id, "getBadge.php?badgeID=");
+        String[] outAry = in.split("\"");
+        return outAry[25];
     }
-    */
+}
+
+//Set of commands for pulling a set of a user's associated badges
+class UserBadgeInfo extends APICaller {
 
 }
 
+//Set of commands for changing user info
+class ChangeUserInfo extends APICaller{
 
+}
+
+//Set of commands for updating
+class UpdateUserBadge extends APICaller{
+
+}
 
 public class Framework extends AppCompatActivity {
 
